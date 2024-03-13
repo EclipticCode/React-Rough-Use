ad9510cf9a650f4268c11269258f4b1612a74c40
daylightcoderj6 <jananibharathi6@gmail.com>
2024-03-13 16:11:08 +0530

    crated an example array

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,31 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
+// data 
+const notes = [
+  {
+   id:1,
+   content :"HTML is easy",
+   date: "2019-05-30T17:30:31.098Z",
+   important : true
+},
+{
+  id:2,
+  content :"JS is easy",
+  date: "2019-05-30T18:30:31.091Z",
+  important : false
+},
+{
+  id:3,
+  content :"CSS is easy",
+  date: "2019-05-30T19:20:14.298Z",
+  important : true
+}
+];
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App notes = { notes } />
   </React.StrictMode>,
 )
