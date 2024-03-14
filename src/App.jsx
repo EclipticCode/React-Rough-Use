84630c61af58bb94bc0b2129de2d9b8492b168ce
daylightcoderj6 <jananibharathi6@gmail.com>
2024-03-14 16:00:30 +0530

    stateful components

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,30 @@ import { Component } from "react"
 import Hello from "./components/Hello"
 
 class App extends Component{
-  render(){
+ constructor(props){
+  super(props)
+
+  // set the initial state
+  this.state = {
+    count : 0
+  }
+ }
+ increment = ()=>{
+  // update the state using the setState method
+  this.setState({
+    count : this.state.count + 1
+  })
+
+}
+ render(){
+  console.log(this.state)
   return(
     <div>
-      <Hello name="Janani"/>
+      <p>Count : { this.state.count }</p>
+      <button onClick={this.increment}>Increment</button>
     </div>
   )
-  }
+ }
 }
 
 export default App
