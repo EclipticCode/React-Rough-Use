aa0d5072492c01990a2279ec78f3ec63baec4bcf
daylightcoderj6 <jananibharathi6@gmail.com>
2024-03-14 11:29:40 +0530

    user component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import User from "./components/User"
 
 const App = () => {
+  
+  const isLoggedIn = false 
   return (
-    <div>App</div>
+    <div>
+       {
+        isLoggedIn ?    
+        ( <User message="Welcome,User !" buttonlabel="Logout"/> ) :
+        ( <User message="Please Login" buttonlabel="Login"/> )
+       }
+
+    </div>
   )
 }
+
+export default App
---
+++ src/components/User.jsx
@@ -0,0 +1,7 @@
+const User = (props)=>{
+    return (
+      <p>{props.message} <button>{props.buttonlabel}</button></p>
+      )
+  }
+
+export default User
